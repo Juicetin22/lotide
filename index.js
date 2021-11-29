8170fb19c56c2aa75f9fea1808ac63688c455391
Justin Huang <justin.2260@gmail.com>
2021-11-29 18:24:12 -0500

    added index.js to export all utility functions

---
+++ index.js
@@ -0,0 +1,25 @@
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+
+module.exports = {
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  letterPositions: letterPositions,
+  map: map
+};
